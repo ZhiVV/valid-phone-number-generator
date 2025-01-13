ad9ec75e7cf952b206083e8f5992836359f35168
Vladislav Zhichkin <vlad.zhichkin@gmail.com>
2025-01-13 21:44:22 +0200

    Added 3 countries for which there are no locales in Mimesis. Small improvements

--- appfront/views.py
+++ appfront/views.py
@@ -5,9 +5,12 @@ import qrcode.image.svg
 # from io import BytesIO
 from appfront.functions import generate_number, make_qr_code
 from mimesis.locales import Locale
+from appfront.config import EXTRA_LOCALE
 
 
 dct_locale = {i.name: i.value for i in Locale}
+for key, value in EXTRA_LOCALE.items():
+    dct_locale[f'{value.country_name} ({value.country_code})'] = key
 
 
 def index(request):
@@ -16,13 +19,9 @@ def index(request):
     }
     if request.GET.get('countryCode'):
         data['country_code'] = request.GET.get('countryCode')
-        # data['quantity'] = request.GET.get('quantity')
         list_of_phonenumbers: PhoneNumber = generate_number(request.GET.get('countryCode'))
         data['phone_number'] = list_of_phonenumbers
         img_qr = make_qr_code(text=list_of_phonenumbers.national_number)
-        # stream = BytesIO()
-        # img_qr.save(stream)
-        # data['svg'] = stream.getvalue().decode()
         data['svg'] = img_qr
 
     return render(request, 'appfront/index.html', data)
